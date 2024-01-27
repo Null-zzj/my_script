3b983cd56db96d5d2e11e4324a791497a12d3580
Null-zzj <zj.zhu.cn@gmail.com>
2024-01-28 00:28:45 +0800

    1

--- install.sh
+++ install.sh
@@ -3,8 +3,16 @@
 # 设置目标目录
 target_directory="$HOME/.my_script"
 
-# 下载整个仓库到目标目录
-git clone https://github.com/Null-zzj/my_script.git "$target_directory"
+# 检查目标目录是否已经存在
+if [ -d "$target_directory" ]; then
+  echo "Target directory $target_directory already exists. Updating repository..."
+  # 如果存在，进入目录并执行git pull以更新仓库
+  (cd "$target_directory" && git pull origin main)
+else
+  echo "Cloning repository to $target_directory..."
+  # 如果不存在，克隆整个仓库到目标目录
+  git clone https://github.com/your_username/your_repository.git "$target_directory"
+fi
 
 # 检查环境变量是否已经存在
 if ! grep -q "$target_directory" "$HOME/.profile"; then
