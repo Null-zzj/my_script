7d56b7aef8463b98a86aba1336dc4b8340cde963
kuraxii <zj.zhu.cn@gmail.com>
2024-08-02 12:40:31 +0800

    1

--- install.sh
+++ install.sh
@@ -9,6 +9,19 @@ install()
   cp -r "config" $MYCONFIG
   cp -r "script" $MYCONFIG
   cp .clang-format $MYCONFIG
+  start_marker="# >>> my profile >>>"
+  line_to_add='. "$HOME/.config/my-profile/config/profile"'
+  end_marker="# >>> wsl profile >>>"
+
+  # 检查行块是否已经存在于~/.bashrc中
+  if ! grep -qF "$line_to_add" ~/.bashrc; then
+      {
+          echo ""
+          echo "$start_marker"
+          echo "$line_to_add"
+          echo "$end_marker"
+      } >> ~/.bashrc
+  fi
 }
 
 install
